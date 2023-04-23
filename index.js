afb4a3cd35a729ba381c370cfdbb7bb1fdcb4453
Yauma9 <pqueijeiro27@gmail.com>
2023-04-23 17:16:07 -0600

    modified:   index.js modified:   package-lock.json modified:   package.json new file:   pokedex.json

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ DELETE
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido al servidor");
 });
 
-app.listen(3000, () => {
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola, " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...")
 })
  
